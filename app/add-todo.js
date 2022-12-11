ee60a7ba3b093f05b1f16f9ab61dd235faf94c80
Stephen Smith <stephenlehanesmith@gmail.com>
2022-12-11 14:51:41 +0800

    seperated logic into seperate components

---
+++ app/add-todo.js
@@ -0,0 +1,8 @@
+export default function AddTodo() {
+  return (
+    <div>
+      <input type="text" />
+      <button>Add</button>
+    </div>
+  )
+}
---
+++ app/todo-list.js
@@ -0,0 +1,11 @@
+export default function TodoList() {
+  return (
+    <div>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        <li style={{ padding: "5px 0" }}>Todo 1</li>
+        <li style={{ padding: "5px 0" }}>Todo 2</li>
+        <li style={{ padding: "5px 0" }}>Todo 3</li>
+      </ul>
+    </div>
+  )
+}
