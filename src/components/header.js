d42c85c4e3bce7dd9773dc2465f7b16bbb3d8ae9
Stephen Smith <stephenlehanesmith@gmail.com>
2022-12-18 17:33:09 +0800

    added image and logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
 import Darkmode from "./darkmode";
+import toad from "../images/toad-124.png";
 
 export default function Header() {
   return (
     <div className="header-cont">
-      <h1>Todo App</h1>
+      <div className="title-image-cont">
+        <h1>Toad-do App</h1>
+        <img src={toad} alt="a toad"></img>
+      </div>
       <Darkmode/>
     </div>
   )
