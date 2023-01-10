2a52fae24fd15aea04f3d29a4359a04802fa85fd
Stephen Smith <stephenlehanesmith@gmail.com>
2023-01-10 12:21:15 +0800

    added underline to items that are completed

--- src/services/todo.js
+++ src/services/todo.js
@@ -1,5 +1,5 @@
-import axios from "axios"
-const baseUrl = "/api/todos"
+import axios from "axios";
+const baseUrl = "/api/todos";
 
 const getAll = (listName) => {
   if (listName) {
@@ -7,28 +7,30 @@ const getAll = (listName) => {
   } else {
   return axios.get(baseUrl)
   }
-}
+};
 
 const create = newObject => {
   return axios.post(baseUrl, newObject)
-}
+};
 
 const update = (id, newObject) => {
   return axios.put(`${baseUrl}/${id}`, newObject)
-}
+};
 
 const deleteOne = id => {
   return axios.delete(`${baseUrl}/${id}`)
-}
+};
 
 const deleteMany = (listName) => {
   return axios.delete(`${baseUrl}?list=${listName}`)
-}
+};
 
-export default {
+const TodoService = {
   getAll,
   create,
   update,
   deleteOne,
   deleteMany,
-}
+};
+
+export default TodoService;
--- src/services/todoLists.js
+++ src/services/todoLists.js
@@ -1,20 +1,22 @@
-import axios from "axios"
-const baseUrl = "/api/todolists"
+import axios from "axios";
+const baseUrl = "/api/todolists";
 
 const getAll = () => {
   return axios.get(baseUrl)
-}
+};
 
 const create = newObject => {
   return axios.post(baseUrl, newObject)
-}
+};
 
 const deleteOne = id => {
   return axios.delete(`${baseUrl}/${id}`)
-}
+};
 
-export default {
+const TodoListService = {
   getAll,
   create,
   deleteOne
-}
+};
+
+export default TodoListService;
